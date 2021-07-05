0c1172f0a5fb3bb0be1d296883fb82c407d93178
ezeilo-su <ezeilo.su@gmail.com>
2021-07-06 00:37:46 +0100

    Add a few examples

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { UUIDV4 } = require('sequelize');
 const Sequelize = require('sequelize');
 
 const app = express();
@@ -10,13 +11,42 @@ const connection = new Sequelize('db', 'user', 'pass', {
   storage: 'db.sqlite',
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on PORT ${port}`);
+const User = connection.define('User', {
+  uuid: {
+    type: Sequelize.UUID,
+    defaultValue: UUIDV4,
+  },
+  name: Sequelize.STRING,
+  bio: Sequelize.TEXT,
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Sunday',
+      bio: 'Software dev',
+    });
+    res.send({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.send('Oops! Something went wrong', err.message);
+  }
 });
 
 connection
-  .authenticate()
+  .sync({
+    force: true,
+    logging: console.log,
+  })
   .then(() => {
     console.log('Connected to sqlite on localhost');
   })
-  .catch((e) => console.log(e));
+  .catch((e) => console.log('Unable to connect to the database', e));
+
+app.listen(port, () => {
+  console.log(`Server listening on PORT ${port}`);
+});
